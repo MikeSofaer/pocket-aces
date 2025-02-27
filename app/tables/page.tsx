5741016c4986e42a47747ff14fca963a26ae2f1c
Michael Sofaer <michaelsofaer@Michaels-MacBook-Pro.local>
2025-02-27 16:00:51 -0700

    Adding buttons

--- app/tables/page.tsx
+++ app/tables/page.tsx
@@ -2,6 +2,31 @@
 
 import { useEffect, useState } from "react";
 import sdk from "@farcaster/frame-sdk";
+import { useWriteContract } from "wagmi";
+
+const abi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "spender", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [{ type: "bool" }],
+  },
+  {
+    type: "function",
+    name: "transferFrom",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "sender", type: "address" },
+      { name: "recipient", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [{ type: "bool" }],
+  },
+] as const;
 
 export default function Page({
   params,
@@ -9,6 +34,7 @@ export default function Page({
   params: Promise<{ contract_id: string }>;
 }) {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const { writeContract } = useWriteContract();
 
   // Initialize Frame SDK
   useEffect(() => {
@@ -31,6 +57,31 @@ export default function Page({
     <div>
       <script>console.log(0); await frame.sdk.actions.ready();</script>
       <div>This is the tables index: {params.toString()}</div>
+      <button
+        onClick={() =>
+          writeContract({
+            abi,
+            address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+            functionName: "placeBet",
+            args: ["1"],
+          })
+        }
+      >
+        Bet on AI 1
+      </button>
+      <br />
+      <button
+        onClick={() =>
+          writeContract({
+            abi,
+            address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+            functionName: "placeBet",
+            args: ["2"],
+          })
+        }
+      >
+        Bet on AI 2
+      </button>
     </div>
   );
 }
